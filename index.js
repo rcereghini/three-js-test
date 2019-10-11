576fa486295e3c838c60e07e9f23c33a762fee2e
rcereghini <robertc@petroleumrx.com>
2019-10-11 13:44:17 -0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ console.log("Hello World from your main file!");
 // import * as THREE from "three";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 var THREE = require("three");
-require("three/examples/js/loaders/GLTFLoader");
+// require("three/examples/js/loaders/GLTFLoader");
+import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
+require("three/examples/js/loaders/OBJLoader");
 
 const mixers = [];
 const clock = new THREE.Clock();
@@ -11,6 +13,9 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(1, 1, 100);
 var renderer = new THREE.WebGLRenderer();
+var textureLoader = new THREE.TextureLoader();
+var map = textureLoader.load("models/low-poly-fox-by-pixelmannen-obj/texture.png");
+var material = new THREE.MeshPhongMaterial({ map: map });
 renderer.setClearColor(0xc5c5c3);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,36 +26,60 @@ document.body.appendChild(renderer.domElement);
 // var cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
-var loader = new THREE.GLTFLoader();
-loader.load("./models/Parrot.glb", function(gltf) {
-	// var mixer = new THREE.AnimationMixer(gltf);
-	// var clips = gltf.animations;
+var loader = new THREE.OBJLoader();
 
-	// function update() {
-	// 	mixer.update(deltaSeconds);
-	// }
+// load a resource
+loader.load(
+	// resource URL
+	"models/low-poly-fox-by-pixelmannen-obj/low-poly-fox-by-pixelmannen.obj",
+	// called when resource is loaded
 
-	const animation = gltf.animations[0];
+	function(object) {
+		object.traverse(function(node) {
+			if (node.isMesh) node.material = material;
+		});
+		scene.add(object);
+	},
+	// called when loading is in progresses
+	function(xhr) {
+		console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+	},
+	// called when loading has errors
+	function(error) {
+		console.log("An error happened");
+	}
+);
 
-	const mixer = new THREE.AnimationMixer(gltf.scene);
-	mixers.push(mixer);
+// var loader = new THREE.GLTFLoader();
+// loader.load("./models/walkingCrab/WalkingCrab.obj", function(gltf) {
+// 	// var mixer = new THREE.AnimationMixer(gltf);
+// 	// var clips = gltf.animations;
 
-	const action = mixer.clipAction(animation);
-	action.play();
+// 	// function update() {
+// 	// 	mixer.update(deltaSeconds);
+// 	// }
 
-	console.log("gltf ===>", gltf);
-	gltf.scene.scale.set(1, 1, 1);
-	gltf.scene.position.x = 0; //Position (x = right+ left-)
-	gltf.scene.position.y = 0; //Position (y = up+, down-)
-	gltf.scene.position.z = 0;
-	scene.add(gltf.scene);
+// 	const animation = gltf.animations[0];
 
-	// var clip = THREE.AnimationClip.findByName(clips, "parrot_A_");
-	// var action = mixer.clipAction(clip);
-	// action.play();
-});
+// 	const mixer = new THREE.AnimationMixer(gltf.scene);
+// 	mixers.push(mixer);
 
-var ambientLight = new THREE.AmbientLight("blue");
+// 	const action = mixer.clipAction(animation);
+// 	action.play();
+
+// 	console.log("gltf ===>", gltf);
+// 	gltf.scene.scale.set(1, 1, 1);
+// 	gltf.scene.position.x = 0; //Position (x = right+ left-)
+// 	gltf.scene.position.y = 0; //Position (y = up+, down-)
+// 	gltf.scene.position.z = 0;
+// 	scene.add(gltf.scene);
+
+// 	// var clip = THREE.AnimationClip.findByName(clips, "parrot_A_");
+// 	// var action = mixer.clipAction(clip);
+// 	// action.play();
+// });
+
+var ambientLight = new THREE.AmbientLight("whitesmoke");
 scene.add(ambientLight);
 
 var directionalLight = new THREE.DirectionalLight(0xffffff);
@@ -60,14 +89,18 @@ scene.add(directionalLight);
 console.log("loader", loader);
 
 console.log("scene ==>", scene);
-// camera.position.z = 5;
+camera.position.z = 5;
+camera.position.y = 25;
 
 var animate = function() {
 	requestAnimationFrame(animate);
 
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
-	// camera.position.z += 0.01;
+	scene.children[2].position.x += 0.01;
+	scene.children[2].rotation.y += 0.001;
+	camera.position.z += 0.05;
+	// console.log("loader ===>", loader);
 
 	const delta = clock.getDelta();
 
